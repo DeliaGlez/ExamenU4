f40555733b95dfd4736f3113e01bfe44989b1a95
DeliaGlez <deliavargas.glez@gmail.com>
2024-11-07 23:28:38 -0700

    AuthController-logout

--- app/AuthController.php
+++ app/AuthController.php
@@ -17,7 +17,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 break;
 
             case 'logout':
-                $authController->logout();
+                $email = $_POST['email'];
+                $authController->logout($email);
                 break;
 
             default:
@@ -62,9 +63,9 @@ class AuthController
             $_SESSION['token'] = $data['data']['token'];
             $_SESSION['user_id'] = $data['data']['id'];  
             $_SESSION['user_data'] = $data['data'];
-            if (!isset($_SESSION['global_token'])) {
-                 $_SESSION['global_token'] = bin2hex(random_bytes(32));
-            }
+            // if (!isset($_SESSION['global_token'])) {
+            //      $_SESSION['global_token'] = bin2hex(random_bytes(32));
+            // }
 			header('Location: ' . BASE_PATH . 'home');
 		} 
 		else {
@@ -74,8 +75,30 @@ class AuthController
 
 	}
 
-	public function logout()
+	public function logout($email)
     {
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/logout',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('email' => $email),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
         session_unset();
         session_destroy();
         header('Location: ' . BASE_PATH . 'login');
