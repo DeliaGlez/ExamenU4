f822e5a7baa4df513909b2e2245955b9d1296996
DeliaGlez <deliavargas.glez@gmail.com>
2024-11-08 00:36:47 -0700

    AuthController-getProfile

--- app/AuthController.php
+++ app/AuthController.php
@@ -105,6 +105,38 @@ class AuthController
         
     }
 
+    public function getProfile(){
+        $userId = isset($_SESSION['user_id']) && !empty($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/' . intval($userId),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $result = json_decode($response, true);
+
+        if (isset($result['data'])) {
+            return $result['data'];
+        }
+        return [];
+	
+    }
+
 
 }
 
