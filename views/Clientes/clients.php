2e5a2b1a80db0de0327cbb52e33853c412f926d9
JoseLuisGL <89991775+JoseLuisGL@users.noreply.github.com>
2024-11-15 00:43:02 -0700

    Clientes lista y agregar - Completados

--- views/Clientes/clients.php
+++ views/Clientes/clients.php
@@ -1,5 +1,25 @@
 <?php 
   include_once "../../app/config.php";
+  include_once "../../app/AuthController.php";
+  include_once "../../app/ClientController.php";
+  
+
+  if(!isset($_SESSION['user_data'])){
+    header('Location: ' .BASE_PATH. '?error=Error de autenticación, inicie sesión.');
+    exit;
+  }
+  else{
+    $authController = new AuthController();
+    $clientController = new ClientController();
+
+    $profileData = $authController->getProfile();
+    $clientData = $clientController->getClients();
+    
+    $clients = $clientData['data'];
+    $user = $profileData['data'];
+    
+  }
+  $error_message = isset($_GET['error']) ? $_GET['error'] : '';
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,6 +35,13 @@
     <!-- [Body] Start -->
 
     <body data-pc-preset="preset-1" data-pc-sidebar-theme="light" data-pc-sidebar-caption="true" data-pc-direction="ltr" data-pc-theme="light">
+    <?php if (!empty($error_message)): ?> 
+      <script> 
+        document.addEventListener('DOMContentLoaded', function() {
+          swal("Error", "<?php echo htmlspecialchars($error_message); ?>", "error").then((value) => { window.location.href = '<?= BASE_PATH ?>'; });;
+        });
+      </script> 
+    <?php endif; ?>
     <?php 
       include "../layouts/sidebar.php";
     ?>
@@ -72,7 +99,7 @@
                           >
                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> </button>
                         </div>
-                        <form onsubmit="return validarFormulario()">
+                        <form method="POST" action="client" enctype="multipart/form-data" onsubmit="return validarFormulario()">
                           <div class="modal-body">
                             <div class="mb-3">
                               <label class="form-label">Nombre</label>
@@ -88,11 +115,11 @@
                             </div>
                             <div class="mb-3">
                               <label class="form-label">Está suscrito?</label>
-                              <input class="form-check-input input-primary" type="checkbox" id="is_suscribed" name="is_suscribed" />
+                              <input class="form-check-input input-primary" type="checkbox" id="is_suscribed" name="is_suscribed" value="2" />
                             </div>
                             <div class="mb-3">
-                              <label for="nivelCliente" class="form-label">Nivel de Cliente</label>
-                              <select id="nivelCliente" name="nivelCliente" class="form-select">
+                              <label for="level_id" class="form-label">Nivel de Cliente</label>
+                              <select id="level_id" name="level_id" class="form-select">
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
@@ -105,6 +132,8 @@
                             <button type="button" class="btn btn-light-danger" data-bs-dismiss="modal">Cerrar</button>
                             <button type="submit" class="btn btn-light-primary">Guardar cambios</button>
                           </div>
+                          <input type="hidden" name="action" value="storeClient"/>
+                          <input type="text" name="global_token" value=<?= $_SESSION['global_token'] ?> hidden>
                         </form>
                       </div>
                     </div>
@@ -127,19 +156,23 @@
                     </thead>
                     <tbody>
                       <!-- Aqui va a ir el ciclo para recorrer los clientes y llenar la tabla -->
-                      <tr>
-                        <td>1</td>
-                        <td>Pepe Jason</td>
-                        <td><a href="" class="link-secondary">pepe@gmail.com</a></td>
-                        <td>6121234567</td>
-                        <td>No</td>
-                        <td>Normal</td>
-                        <td>
-                          <a href="<?= BASE_PATH ?>clients_edit" class="btn btn-sm btn-light-success me-1"><i class="feather icon-edit"></i></a>
-                          <a href="" class="btn btn-sm btn-light-danger"><i class="feather icon-trash-2"></i></a>
-                          <a href="<?= BASE_PATH ?>clients_info" class="btn btn-sm btn-light-info"><i class="feather icon-eye"></i></a>
-                        </td>
-                      </tr>
+                      <?php if (!empty($clients)): ?>
+                        <?php foreach ($clients as $client): ?>
+                        <tr>
+                          <td><?= htmlspecialchars($client['id']) ?></td>
+                          <td><?= htmlspecialchars($client['name']) ?></td>
+                          <td><a href="mailto:<?= htmlspecialchars($client['email']) ?>" class="link-secondary"><?= htmlspecialchars($client['email']) ?></a></td>
+                          <td><?= htmlspecialchars($client['phone_number']) ?></td>
+                          <td><?= htmlspecialchars($client['is_suscribed']) ? 'Sí' : 'No' ?></td>
+                          <td><?= htmlspecialchars($client['level']['name']) ?></td>
+                          <td>
+                            <a href="<?= BASE_PATH ?>clients_edit?id=<?= htmlspecialchars($client['id']) ?>" class="btn btn-sm btn-light-success me-1"><i class="feather icon-edit"></i></a>
+                            <a href="" class="btn btn-sm btn-light-danger"><i class="feather icon-trash-2"></i></a>
+                            <a href="<?= BASE_PATH ?>clients_info?id=<?= htmlspecialchars($client['id']) ?>" class="btn btn-sm btn-light-info"><i class="feather icon-eye"></i></a>
+                          </td>
+                        </tr>
+                        <?php endforeach; ?>
+                      <?php endif; ?>
                     </tbody>
                   </table>
                 </div>
@@ -199,6 +232,9 @@
 
         return true;
       }
+      document.getElementById("is_suscribed").addEventListener("change", function() {
+        this.value = this.checked ? "1" : "2";
+      });
     </script>
 
     <?php 
@@ -219,12 +255,13 @@
 
     ?>
  <!-- Required Js -->
-<script src="../../assets/js/plugins/popper.min.js"></script>
-<script src="../../assets/js/plugins/simplebar.min.js"></script>
-<script src="../../assets/js/plugins/bootstrap.min.js"></script>
-<script src="../../assets/js/fonts/custom-font.js"></script>
-<script src="../../assets/js/pcoded.js"></script>
-<script src="../../assets/js/plugins/feather.min.js"></script>
+<script src="<?= BASE_PATH ?>assets/js/plugins/popper.min.js"></script>
+<script src="<?= BASE_PATH ?>assets/js/plugins/simplebar.min.js"></script>
+<script src="<?= BASE_PATH ?>assets/js/plugins/bootstrap.min.js"></script>
+<script src="<?= BASE_PATH ?>assets/js/fonts/custom-font.js"></script>
+<script src="<?= BASE_PATH ?>assets/js/pcoded.js"></script>
+<script src="<?= BASE_PATH ?>assets/js/plugins/feather.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 
   </body>
