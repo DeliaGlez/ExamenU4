565c3b1a0ef46dda246377284a0aa485216d6310
DeliaGlez <deliavargas.glez@gmail.com>
2024-11-21 01:03:43 -0700

    LevelController- getbyId

--- app/LevelController.php
+++ app/LevelController.php
@@ -32,6 +32,32 @@ class LevelController
         return $this->returnToFront($result, 4); 
     }
 
+    public function getLevel($id){
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/levels/'. intval($id),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $result = json_decode($response, true);
+        return $this->returnToFront($result, 4); 
+    }
+
     private function returnToFront($data, $code){
         if (isset($data['code']) && $data['code'] === intval($code)) { 
             return [ 
